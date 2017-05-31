f92d08b8c54bf506a6d9fbbb2eb90b00263b04f2
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-31 15:30:01 -0700

    Add Task class & table cell

---
+++ Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  One Thing
+//
+//  Created by Robert Hatfield on 5/31/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var text : String
+    var isSelected = false
+    
+    init(text: String) {
+        self.text = text
+    }
+}
